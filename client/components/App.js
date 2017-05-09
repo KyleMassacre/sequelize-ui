749d9dfce02db67ce1000f71d01877db8c153407
Tom Schuster <tomjschuster@gmail.com>
2017-05-09 00:05:32 -0400

    convert app wrapper to react-toolbox

--- client/components/App.js
+++ client/components/App.js
@@ -4,34 +4,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { requestDbDownload } from '../redux/models'
 
-import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton'
-import IconButton from 'material-ui/IconButton'
-import NewFolder from 'material-ui/svg-icons/file/create-new-folder'
+import { Layout, Panel } from 'react-toolbox'
+import AppBar from 'react-toolbox/lib/app_bar'
+import FontIcon from 'react-toolbox/lib/font_icon'
 
 export class App extends Component {
   render() {
     let { children, models } = this.props
     return (
-      <div>
-          <AppBar title='Sequelize UI'
-                  iconElementLeft={<IconButton><NewFolder /></IconButton>}
-                  iconElementRight={
-                    <FlatButton onClick={() => requestDbDownload(models)} label='Download Models'/>
-                  }/>
-        <div id='main'>
+      <Layout>
+        <Panel>
+          <AppBar
+            title='Sequelize UI'
+            leftIcon={<FontIcon value='create_new_folder' />}
+            rightIcon={<FontIcon value='file_download' />}
+            onRightIconClick={() => requestDbDownload(models)}
+          />
+          <div style={{ flex: 1, overflowY: 'auto', padding: '0.5rem' }}>
           { children }
-        </div>
-      </div>
+          </div>
+        </Panel>
+      </Layout>
     )
   }
 }
 
 
-const mapStateToProps = ({ models }) => ({ models })
-const mapDispatchToProps = () => ({})
+const mapStateToProps = state => state
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default connect(mapStateToProps)(App)
--- client/index.js
+++ client/index.js
@@ -7,15 +7,19 @@ import store from './store'
 import Routes from './Routes'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import theme from './theme'
+import { ThemeProvider } from 'react-css-themr'
 import indigoTealTheme from './themes/indigoTeal'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
 render(
-  <MuiThemeProvider muiTheme={getMuiTheme(indigoTealTheme)}>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </MuiThemeProvider>,
+  <ThemeProvider theme={theme}>
+    <MuiThemeProvider muiTheme={getMuiTheme(indigoTealTheme)}>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </MuiThemeProvider>
+  </ThemeProvider>,
   document.getElementById('app')
 )
